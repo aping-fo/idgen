82a44e5ac65c70bccab932e31cc7301288763fa2
aping.foo <125455364@qq.com>
2017-05-04 09:00:52 +0800

    'idgen'

---
+++ SeqType.java
@@ -0,0 +1,172 @@
+package com.foo
+/**
+ * 用于t_p_seq中记录数据表的类型
+
+ */
+public interface SeqType
+{
+    // 帐号表
+    public static final int T_U_PLAYER = 1;
+
+    // 物品表
+    public static final int T_U_ITEM = 2;
+
+    // 离线操作表
+    public static final int T_U_HISTORY = 3;
+
+    // 活动条件表（未用）
+    public static final int T_U_ACTIVITY_CONDITION = 4;
+
+    // 公会表（未用）
+    public static final int T_U_GUILD = 5;
+
+    // 新手表
+    public static final int T_U_NEWBIE = 6;
+
+    // 技能表
+    public static final int T_U_SKILL = 7;
+
+    // 强化表
+    public static final int T_U_STRENGHTEN = 8;
+
+    // 好友表
+    public static final int T_U_FRIEND = 9;
+
+    // 宠物图鉴表
+    public static final int T_U_PET_GUIDE = 10;
+
+    // 宠物材料表
+    public static final int T_U_PET_MATERIAL = 11;
+
+    // 多人冒险表
+    public static final int T_U_TEAM_MISSION = 12;
+
+    // 活动副本表
+    public static final int T_U_ACT_MISSION = 13;
+
+    // 商城兑换表
+    public static final int T_U_TMALL_EXCAHNGE = 14;
+
+    // 玩家邮件表
+    public static final int T_U_MAIL_PLAYER = 15;
+
+    // 系统邮件表
+    public static final int T_U_MAIL_SYSTEM = 16;
+
+    // 勋章商城
+    public static final int T_U_MEDAL_SHOP = 17;
+
+    // 深渊关卡
+    public static final int T_U_ASSAULT_MISSION = 18;
+
+    // 任务表
+    public static final int T_U_TASK = 19;
+
+    // 天梯表
+    public static final int T_U_LADDER = 20;
+
+    // 武器属性表
+    public static final int T_U_WEAPON_PROPERTY = 21;
+
+    // 章节宝箱表
+    public static final int T_U_CHAPTER_BOX = 22;
+
+    // 达人表
+    public static final int T_U_MASTER = 23;
+
+    // 达人记录表
+    public static final int T_U_MASTER_RECORD = 24;
+
+    // 时间类型
+    public static final int T_U_EVENT = 25;
+
+    // 套装方案表
+    public static final int T_U_FASHION = 26;
+
+    // 新手训练营
+    public static final int T_U_TRAIN_MISSION = 27;
+
+    // 成长
+    public static final int T_U_GROW_UP = 28;
+
+    //限时副本
+    public static final int T_U_TIME_MISSION_LIMIT = 29;
+
+    // 宝珠背包表
+    public static final int T_U_ORB_BAG = 30;
+
+    // 宝珠方案表
+    public static final int T_U_ORB_PLAN = 31;
+
+    // 公会信息
+    public static final int T_U_GUILD_INFO = 32;
+
+    // 公会事件
+    public static final int T_U_GUILD_EVENT = 33;
+
+    // 公会申请
+    public static final int T_U_GUILD_APPLY = 34;
+
+    // 公会商店
+    public static final int T_U_GUILD_SHOP = 35;
+
+    // 商城
+    public static final int T_U_MALL = 36;
+
+    // 系统公告和走马灯
+    public static final int T_P_ANNOUNCEMENT = 37;
+
+    // 活动系统
+    public static final int T_U_ACTIVITY = 38;
+
+    // 区服随机出来的公会活动
+    public static final int T_U_QUEST = 39;
+
+    // 公会的活动数据
+    public static final int T_U_GUILD_QUEST = 40;
+
+    // 用户参与公会的活动数据
+    public static final int T_U_GUILD_QUEST_PLAYER = 41;
+
+    // 活动(活动模板)
+    public static final int T_S_ACTIVITY = 42;
+
+    // 活动task模板
+    public static final int T_S_ACTIVITY_TASK = 43;
+
+    // 宠物抽取
+    public static final int T_S_PET_EXTRACT = 46;
+
+    // AQ IDIP消息
+    public static final int T_S_AQ_IDIP_MSG = 47;
+
+    // AQ IDIP LIMIT
+    public static final int T_S_AQ_IDIP_LIMIT = 48;
+
+    // 称号
+    public static final int T_U_TITLE = 49;
+
+    // 称号条件统计
+    public static final int T_U_TITLE_CONDITION_STAT = 50;
+
+    // 头像框
+    public static final int T_U_HEAD_FRAME = 51;
+
+    // 充值
+    public static final int T_U_RECHARGE = 52;
+
+    // 达人奖励
+    public static final int T_U_CHAMPION = 53;
+
+    // 时装图鉴
+    public static final int T_U_FASHION_ILLUSTARTE = 54;
+
+    // 资源找回
+    public static final int T_U_RESOURCE_RECOVERY = 55;
+    
+    // 好友送礼记录
+    public static final int T_U_PLAYER_GIFT = 56;
+    
+    // seq 值数量
+    public static final int NUM_SEQ = 57;
+}
---
+++ UUIDMgr.java
@@ -0,0 +1,161 @@
+package com.foo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.foo.SeqType;
+public class UUIDMgr
+{
+    /**
+     * 日志记录
+     */
+    private static final Logger Log = LoggerFactory.getLogger(UUIDMgr.class.getName());
+
+    /**
+     * 每次获取数量
+     */
+    private static final int uniqueIdAdd = 100;
+
+    /**
+     * 键值表格
+     */
+    private static Map<Integer, UniqueIdData> uniqueIdsMap = new HashMap<>();
+
+    /**
+     * 服务器进程id,保持唯一
+     */
+    private static final int serverId = GlobalConfigManager.getInstance().getServerId();
+
+//    /**
+//     * 键值表格
+//     */
+//    private static Map<Integer, UniqueIdIntData> uniqueIdIntsMap = new HashMap<>();
+
+    public static boolean init()
+    {
+        return reload();
+    }
+
+    private static int generateSeqKey(int i)
+    {
+        return serverId + i * 10000;
+    }
+    
+    public static boolean reload()
+    {
+        try
+        {
+            uniqueIdsMap.clear();
+//            uniqueIdIntsMap.clear();
+
+            for (int i = 1; i < SeqType.NUM_SEQ; i++)
+            {
+                int key = generateSeqKey(i);
+                SeqData seqData = DaoManager.getSeqTableDao().getBySeqType(key);
+                if (seqData == null)
+                {
+                    seqData = new SeqData();
+                    seqData.setSeqType(key);
+                    seqData.setValue(100);
+                    DaoManager.getSeqTableDao().add(seqData);
+                }
+                
+                uniqueIdsMap.put(seqData.getSeqType(), new UniqueIdData(seqData.getSeqType()));
+            }
+
+            return true;
+        }
+        catch (Exception e)
+        {
+            Log.error("UUIDMgr init exception", e);
+            return false;
+        }
+    }
+
+    /**
+     * @param seqType
+     * @return
+     */
+    public static long getCurrentId(int seqType)
+    {
+        int keyId = generateSeqKey(seqType);
+        long uuid = serverId + uniqueIdsMap.get(keyId).value() * 10000L;
+        return uuid;
+    }
+
+//    public static int getIntegerCurrentId(int seqType)
+//    {
+//        int keyId = generateSeqKey(seqType);
+//        int uuid = uniqueIdIntsMap.get(keyId).value();
+//        return uuid;
+//    }
+
+    private static long getLastId(int seqType)
+    {
+        return DaoManager.getSeqTableDao().getByKey(seqType).getValue();
+    }
+
+//    private static int getIntLastId(int seqType)
+//    {
+//        return DaoManager.getSeqTableDao().getByKey(seqType).getValue();
+//    }
+
+    private static class UniqueIdData
+    {
+        /**
+         * 索引类型
+         */
+        private int uniqueId;
+
+        /**
+         * id计数器
+         */
+        private long currentId;
+
+        /**
+         * 最大索引
+         */
+        private long upperLimit;
+
+        /**
+         * 同步对象
+         */
+        private Object lock = new Object();
+
+        private UniqueIdData(int uniqueId)
+        {
+            this.uniqueId = uniqueId;
+            this.currentId = 0;
+            this.upperLimit = 0;
+
+            update();
+        }
+
+        /**
+         * @return
+         */
+        public long value()
+        {
+            synchronized (lock)
+            {
+                long value = ++currentId;
+                if (value >= upperLimit)
+                    update();
+
+                return value;
+            }
+        }
+
+        /**
+         * 每次获取指定数量100索引
+         */
+        private void update()
+        {
+            upperLimit = getLastId(uniqueId);
+            currentId = upperLimit - uniqueIdAdd;
+        }
+    }
+}
